aeccdbe885c3af7685a4ac856e60720f099e5342
Steffen <steffaeaeaen@gmail.com>
2021-05-28 15:52:02 +0200

    add queueItems argument

--- parser.js
+++ parser.js
@@ -2,6 +2,7 @@ const {UserError} = require("./error");
 const RANGE_REGEX = /^[0-9]+-[0-9]+$/;
 const NUMBER_REGEX = /^[0-9]+$/;
 const USER_REGEX = /^<@!?(\d+)>$/;
+const LIST_REGEX = /^((\d+)|(\d+-\d+))(,((\d+)|(\d+-\d+)))*$/;
 
 async function getParsed(expectedArg, arg) {
     if (!arg) {
@@ -9,8 +10,8 @@ async function getParsed(expectedArg, arg) {
     }
 
     switch (expectedArg.type) {
-        case "queueItem":
-            return parseQueueItem(expectedArg, arg);
+        case "queueItems":
+            return parseQueueItems(expectedArg, arg);
         case "user":
             return parseUser(expectedArg, arg);
         case "value":
@@ -32,10 +33,74 @@ async function getParsed(expectedArg, arg) {
     }
 }
 
-function parseQueueItem(expected, arg) {
+async function parseQueueItems(expected, arg) {
+    let parsingFunctions = [parseQueueItemsAsList, /*parseQueueItemsFromName,*/ parseQueueItemsFromMember, parseQueueItemsFromRegex];
+    let value;
+
+    for (let parsingFunction of parsingFunctions) {
+        try {
+            value = await parsingFunction(expected, arg);
+        } catch (e) {
+            // error while parsing with the current function, try the next one
+            continue;
+        }
+        // the input was parsed by the current function, break out of the loop
+        console.log("the input was correctly parsed by " + parsingFunction.name);
+        break;
+    }
+
+    if (!value) {
+        // none of the functions could parse the input
+        throw new UserError("Failed to parse the queue item description");
+    }
+
+    return value;
+}
+
+async function parseQueueItemsAsList(expected, arg) {
+    if (!LIST_REGEX.test(arg)) {
+        // not a list
+        throw new UserError("The input is not a list");
+    }
     expected.min = 1;
     expected.max = musicplayer.getQueueLength();
-    return parsePositiveInteger(expected, arg);
+    return parseList(expected, arg).map(i => i - 1);
+}
+
+async function parseQueueItemsFromName(expected, arg) {
+    let indices = [];
+
+}
+
+async function parseQueueItemsFromRegex(expected, arg) {
+    let regex = new RegExp(arg, "i");
+    let indices = [];
+    for (let i = 0; i < musicplayer.queue.getSongAmount(); i++) {
+        if (regex.test(musicplayer.queue.getQueue()[i].title)) {
+            console.log(`${i} matches`);
+            indices.push(i);
+        }
+    }
+    if (indices.length === 0) {
+        throw new UserError("None of the titles matched");
+    }
+
+    return indices;
+}
+
+async function parseQueueItemsFromMember(expected, arg) {
+    let user = await parseUser(expected, arg);
+    let indices = musicplayer.queue.getQueue().reduce((accu, curr, index) => {
+        if (curr.member.id === user.id) {
+            accu.push(index);
+        }
+    }, []);
+
+    if (indices.length === 0) {
+        throw new UserError("The are no songs from that user in the queue");
+    }
+
+    return indices;
 }
 
 async function parseUser(expected, arg) {
